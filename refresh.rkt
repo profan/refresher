01bf970a7601e170d2f02c9d4997a84aaa1c12e1
Robin Hübner <profan@prfn.se>
2015-08-27 23:48:14 +0200

    Progress!

--- refresh.rkt
+++ refresh.rkt
@@ -14,6 +14,10 @@
 (require web-server/servlet
          web-server/servlet-env)
 
+; global variables
+(define page-content (string->xexpr "<html></html>"))
+(permissive-xexprs #t)
+
 ; command line arguments config
 (define index-file (make-parameter "index.html"))
 (define watched-directory (make-parameter "."))
@@ -28,31 +32,38 @@
                           "Directory to watch for changes."
                           (watched-directory dir)])
 
+; utility functions
 (define (inject-listener in-xml)
-  (define read-xml (read-xml in-xml))
-  (displayln read-xml))
+  (define xml-data (read-xml in-xml))
+  (xml->xexpr (document-element xml-data)))
 
 (define (reload-index file-name)
-  (call-with-input-file file-name)
-    (lambda (in) ((inject-listener in))))
+  (call-with-input-file file-name
+    (lambda (in) (inject-listener in))))
+
+(define (notify-listener remote-address remote-port notification)
+  (define addr (string-append (~a remote-address) (~a remote-port)))
+  (define c (ws-connect (string->url addr)))
+  (ws-send! c notification))
 
 ; listeners
 (define (change-listener directory target-thread)
-  (define change-event (filesystem-change-evt directory))
+    (define change-event (filesystem-change-evt directory))
   (define change (sync change-event)) ; sync on change - do things when shit happens!
   (thread-send target-thread #t)
   (change-listener directory target-thread))
 
-(define (listen index directory)
-
-  #t)
+(define (page-servlet req)
+  (response/xexpr page-content))
 
 (define (start-listener index directory)
   (define this-thread (current-thread))
   (define change-thread (thread (lambda () (change-listener directory this-thread))))
+  (define servlet-thread (thread (lambda () (serve/servlet page-servlet))))
   (define (do-listener)
     (define received-event (thread-receive))
-    (displayln (format "event: ~s" received-event))
+    (displayln (format "listener got event: ~s" received-event))
+    (set! page-content (reload-index index))
     (do-listener))
   (do-listener))
 
