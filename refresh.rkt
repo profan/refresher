e23a89bb2df55d8b0d121695dd7d32725a3a7753
Robin Hübner <profan@prfn.se>
2015-08-28 01:00:52 +0200

    Almost functional.

--- refresh.rkt
+++ refresh.rkt
@@ -8,11 +8,15 @@
 
 #lang racket
 
-(require xml)
+(require xml xml/path)
 (require net/rfc6455)
 (require racket/cmdline)
 (require web-server/servlet
          web-server/servlet-env)
+
+; constants?
+(define web-port 8000)
+(define ws-port 8001)
 
 ; global variables
 (define page-content (string->xexpr "<html></html>"))
@@ -21,6 +25,21 @@
 ; command line arguments config
 (define index-file (make-parameter "index.html"))
 (define watched-directory (make-parameter "."))
+
+; injected code
+(define injected-code 
+  "<script>
+  (function() {
+    var ws = new WebSocket(\"ws://localhost:8001\");
+
+    ws.onmessage = function(evt) {
+      window.location.reload();
+    }
+
+   })();
+  </script>")
+
+(define injected-xml (string->xexpr injected-code))
 
 (command-line
     #:program "refresher"
@@ -35,16 +54,12 @@
 ; utility functions
 (define (inject-listener in-xml)
   (define xml-data (read-xml in-xml))
-  (xml->xexpr (document-element xml-data)))
+  (define xml-structure (xml->xexpr (document-element xml-data)))
+  (append xml-structure (list injected-xml)))
 
 (define (reload-index file-name)
   (call-with-input-file file-name
     (lambda (in) (inject-listener in))))
-
-(define (notify-listener remote-address remote-port notification)
-  (define addr (string-append (~a remote-address) (~a remote-port)))
-  (define c (ws-connect (string->url addr)))
-  (ws-send! c notification))
 
 ; listeners
 (define (change-listener directory target-thread)
@@ -53,17 +68,32 @@
   (thread-send target-thread #t)
   (change-listener directory target-thread))
 
+(define (websocket-listener port owner)
+  (thread-send owner (current-thread))
+  (ws-serve #:port port handle-connection))
+
+(define (handle-connection c state)
+  (define sent-event (sync (thread-receive-evt)))
+  (ws-send! c (~a sent-event)))
+
 (define (page-servlet req)
   (response/xexpr page-content))
 
 (define (start-listener index directory)
+  (set! page-content (reload-index index))
   (define this-thread (current-thread))
   (define change-thread (thread (lambda () (change-listener directory this-thread))))
   (define servlet-thread (thread (lambda () (serve/servlet page-servlet))))
+  (define websocket-thread (thread (lambda() (websocket-listener ws-port this-thread))))
+  (define client-thread '())
   (define (do-listener)
     (define received-event (thread-receive))
-    (displayln (format "listener got event: ~s" received-event))
-    (set! page-content (reload-index index))
+    (cond
+      [(thread? received-event) (set! client-thread received-event)]
+      [(thread? client-thread)
+      (displayln (format "listener got event: ~s" received-event))
+      (thread-send client-thread received-event)
+      (set! page-content (reload-index index))])
     (do-listener))
   (do-listener))
 
